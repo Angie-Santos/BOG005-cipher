6797832ccef6c47fab01bee18eb6526f6a67d3e2
Angie-Santos <santosangie79@gmail.com>
2022-07-15 08:17:57 -0500

    3 versión Funciones JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
 const cipher = {
-  // ...
-};
+  encode(offset, string){
+/*
+    var offsetString = offset%26;
+    var newString = "";
+    for (let i = 0; i < string.length; i++) {
+      newString += String.fromCharCode(string[i].charCodeAt()+offsetString);
+    }
+   */ 
 
-export default cipher;
+    //var offsetString = offset%26;
+    var newString = "";
+    for (let i = 0; i < string.length; i++) {
+      newString += String.fromCharCode(((string[i].charCodeAt())-65+offset)%26+65);
+    }
+    return newString;
+  },
+  decode(offset, string){
+  
+  var newStringDecode = "";
+    for (let i = 0; i < string.length; i++) {
+      newStringDecode += String.fromCharCode(((string[i].charCodeAt())+65-offset)%26+65);
+    }
+    console.log(newStringDecode);
+    return newStringDecode;
+  } 
+
+}; export default cipher; 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
-    
-console.log(cipher);
+
+//console.log(cipher);
+
+//Pasar de displays
+
+const buttonContinue = document.getElementById("buttonContinue");
+buttonContinue.addEventListener("click",changeDisplay);
+
+function changeDisplay(){
+    document.getElementById("Welcome").style.display="none";
+    document.getElementsByTagName("main")[0].style.display="block";
+}
+
+//obtener las variables del formulario
+
+
+const form = document.formulario;
+form.onsubmit = function (event){
+    event.preventDefault();
+    var offset = form.offset.value;
+    var string = form.textBox1.value;
+    cipher.decode(offset,string)
+}
+
+
+
+
